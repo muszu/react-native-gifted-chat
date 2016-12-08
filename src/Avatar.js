829548d13c0a3054a7b3e0133a7f870c02246195
marcin.muszalski <marcin.muszalski@swmansion.com>
2016-12-08 10:37:02 +0100

    Add system messages

--- src/Avatar.js
+++ src/Avatar.js
@@ -52,6 +52,16 @@ const styles = {
       borderRadius: 18,
     },
   }),
+  center: StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+    },
+    image: {
+      height: 36,
+      width: 36,
+      borderRadius: 18,
+    },
+  }),
   right: StyleSheet.create({
     container: {
       marginLeft: 8,
@@ -78,7 +88,7 @@ Avatar.defaultProps = {
 };
 
 Avatar.propTypes = {
-  position: React.PropTypes.oneOf(['left', 'right']),
+  position: React.PropTypes.oneOf(['left', 'center', 'right']),
   currentMessage: React.PropTypes.object,
   nextMessage: React.PropTypes.object,
   containerStyle: View.propTypes.style,
